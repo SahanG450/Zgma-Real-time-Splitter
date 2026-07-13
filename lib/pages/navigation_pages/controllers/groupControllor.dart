ba01005ddf37e62f88767ac374e1937bbcf26f84
sahan450G <sahangajanayake450@gmail.com>
2026-07-13 10:15:24 +0530

    add group geting functions base on the userid

--- lib/pages/navigation_pages/controllers/groupControllor.dart
+++ lib/pages/navigation_pages/controllers/groupControllor.dart
@@ -1,15 +1,24 @@
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-Future<SessionCreateResult> getGroups(string userid) async {
+Future<List<String>> getGroups(String userid) async {
   late final http.Response response;
   try {
-    response = await http.get(
+    // Note: get() doesn't support a body in the http package. 
+    // If your API requires a body, use post().
+    response = await http.post(
       Uri.parse('http://10.0.2.2:3000/api/session/getGroup'),
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(userid.toJson()),
+      body: jsonEncode({'userId': userid}),
     );
   } catch (e) {
     throw Exception('Could not reach the server. Check your connection and try again.');
   }
 
+  if (response.statusCode == 200) {
+    final List<dynamic> data = jsonDecode(response.body);
+    return data.map((group) => group.toString()).toList();
+  } else {
+    throw Exception('Failed to load groups. Status: ${response.statusCode}');
+  }
 }
